f9d7d3f61a35298840cb7eda4fad0661463d65d5
Foxyf76 <lukefox5@gmail.com>
2019-06-04 00:32:33 +0100

    V2, improved functionality

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
-import os
+
 import sys
 import time
-import webbrowser
-
 import pyautogui
-from pynput.mouse import Button, Controller
+from PIL import Image
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-from PIL import Image
-
 browser = webdriver.Chrome(ChromeDriverManager().install())
-
 pyautogui.FAILSAFE = True
 game_mode = ' '.join(sys.argv[1:])
 pyautogui.PAUSE = 0
 
+start_x = 750
+stary_y = 690
+
+bottom_row = (start_x, stary_y, start_x + 400, 1)
+middle_row = (start_x, stary_y - 150, start_x + 400, stary_y + 1)
+top_row = (start_x, stary_y - 300, start_x + 400, stary_y + 1)
+
+black = (19, 19, 19)
+red = (248, 52, 56)
+grey = (157, 157, 157)
+
+score = 0
+y_adjustment = 30
+previous_lane = 0
+
 
 def main():
+    select_mode(game_mode)
+    time.sleep(3)
+    click_bottom_tiles() # start the game
+    while 0<1:
+        click_middle_tiles()
+
+
+def select_mode(game_mode):
     browser.get('http://tanksw.com/piano-tiles/')
     browser.maximize_window()
-    select_mode(game_mode)
+    if game_mode == 'classic':
+        classic_mode = browser.find_element_by_xpath("//*[@id='menu']/div[1]").click()
 
-    start = time.time()
-
-    while 0 < 1:
-        black_tile = get_black_tile()
+    if game_mode == 'arcade':
+        arcade_mode = browser.find_element_by_xpath("//*[@id='menu']/div[2]").click()
 
 
 def get_pixel(image, i, j):
@@ -36,37 +53,123 @@ def get_pixel(image, i, j):
     return pixel
 
 
-def select_mode(game_mode):
-    if game_mode == 'classic':
-        classic_mode = browser.find_element_by_xpath("//*[@id='menu']/div[1]").click()
-
-    if game_mode == 'arcade':
-        arcade_mode = browser.find_element_by_xpath("//*[@id='menu']/div[2]").click()
-
-
-def get_black_tile():
-    i = 0
-    x = 44
-    y = 400
-    black_pixel = []
-
-    region_image = pyautogui.screenshot(region=(750, 280, 400, 600))
+def click_bottom_tiles():
+    global y_adjustment, black, red, previous_lane
+    starting_pos = 50
+    bottom_image = pyautogui.screenshot(region=bottom_row)
     for i in range(4):
-        pixel = get_pixel(region_image, x, y)
-        next_pixel = get_pixel(region_image, x, y - 10)
-        if pixel == (19, 19, 19):
-            if next_pixel == (19, 19, 19):
-                black_pixel = [x+750, y +280]
-                play_game(black_pixel)
+        # to-do --> count how many times code is looped here and devide score by that amount each time
+        pixel = get_pixel(bottom_image, starting_pos, 0)
+        if pixel == black:
+            click(start_x + starting_pos, stary_y + y_adjustment)
+            break
+        elif pixel == red or pixel == grey:
+            print('YOUR SCORE ', score)
+            sys.exit()
         else:
-            x = x + 100
+            starting_pos = starting_pos + 100  # move to next tile
 
 
-def play_game(black_pixel):
-    x = int(black_pixel[0])
-    y = int(black_pixel[1])
-    pyautogui.moveTo(x, y+100)
-    pyautogui.click(x, y + 48)
+def click_middle_tiles():
+    global y_adjustment, black, red, previous_lane
+    starting_pos = 50
+    middle_image = pyautogui.screenshot(region=middle_row)
+    for i in range(4):
+        pixel = get_pixel(middle_image, starting_pos, 0)
+        if pixel == (19, 19, 19):
+            click(start_x + starting_pos, (stary_y-150) + y_adjustment)
+            print('clicking ', (stary_y-150) + y_adjustment)
+            break
+        elif pixel == red or pixel == grey:
+            print('YOUR SCORE ', round(score/3))
+            sys.exit()
+        else:
+            starting_pos = starting_pos + 100  # move to next tile
+
+# def click_bottom_tiles():
+#     global y_adjustment, black, red, current_pixel
+#     starting_pos = 50
+#     bottom_image = pyautogui.screenshot(region=bottom_row)
+#
+#     pixel1 = get_pixel(bottom_image, starting_pos, 0)
+#     pixel2 = get_pixel(bottom_image, starting_pos+100, 0)
+#     pixel3 = get_pixel(bottom_image, starting_pos+200, 0)
+#     pixel4 = get_pixel(bottom_image, starting_pos+300, 0)
+#
+#     if pixel1 == black:
+#         pyautogui.moveTo(start_x + starting_pos, stary_y + y_adjustment)
+#         if pyautogui.position() == current_lane:
+#             print('CURRENT LANE: ', current_lane)
+#             print('MOUSE: ' , pyautogui.position())
+#             pass
+#         else:
+#             click(start_x + starting_pos, stary_y + y_adjustment)
+#     elif pixel2 == black:
+#         pyautogui.moveTo(start_x + starting_pos+100, stary_y + y_adjustment)
+#         if pyautogui.position() == current_lane:
+#             print('CURRENT LANE: ', current_lane)
+#             print('MOUSE: ' , pyautogui.position())
+#             pass
+#         else:
+#             click(start_x + starting_pos+100, stary_y + y_adjustment)
+#     elif pixel3 == black:
+#         pyautogui.moveTo(start_x + starting_pos+200, stary_y + y_adjustment)
+#         if pyautogui.position() == current_lane:
+#             print('CURRENT LANE: ', current_lane)
+#             print('MOUSE: ' , pyautogui.position())
+#             pass
+#         else:
+#             click(start_x + starting_pos+200, stary_y + y_adjustment)
+#     elif pixel4 == black:
+#         pyautogui.moveTo(start_x + starting_pos+300, stary_y + y_adjustment)
+#         if pyautogui.position() == current_lane:
+#             print('CURRENT LANE: ', current_lane)
+#             print('MOUSE: ' , pyautogui.position())
+#             pass
+#         else:
+#             click(start_x + starting_pos+300, stary_y + y_adjustment)
+
+
+
+def click(x, y):
+    global score
+    pyautogui.click(x, y)
+    score += 1
+    click_adjustment(score)
+
+#
+#
+# def click_top_tiles():
+#     starting_pos = 50
+#     top_image = pyautogui.screenshot(region=top_row)
+#     for i in range(4):
+#         pixel = get_pixel(top_image, starting_pos, 0)
+#         if pixel == (19, 19, 19):
+#             pyautogui.moveTo(start_x + starting_pos, stary_y + 760)
+#             pyautogui.click(start_x + starting_pos, stary_y - 270, clicks=1)
+#             break
+#         else:
+#             starting_pos = starting_pos + 100  # move to next tile
+#     click_middle_tiles()
+
+def click_adjustment(score):
+    global y_adjustment
+    if round(score/3) == 100:
+        y_adjustment += 11
+
+    if round(score/3) == 200:
+        y_adjustment += 22
+
+    if round(score/3) == 400:
+        y_adjustment += 33
+
+    if round(score/3) == 800:
+        y_adjustment += 44
+
+    if round(score/3) == 1600:
+        y_adjustment += 55
+        
+    return y_adjustment
 
 
 if __name__ == "__main__":
